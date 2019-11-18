ed251c6c3db8186476b0185e7c3f36a9fcc9f78c
shareef-sanaullah <57405273+shareef-sanaullah@users.noreply.github.com>
2019-11-18 23:16:40 +0300

    Add files via upload

---
+++ B6_PY_Day9.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+def greet_user(username):
+    """ Display a greeting message to the user"""
+print(f" Hello, {username.title()}welcomeback")
+greet_user('sharEEF')
+
+
+# In[12]:
+
+
+def Describe_pet(animal_type,pet_name):
+    """Display information about a pet"""
+print(f"I have a {animal_type}")
+print(f"\n my {animal_type} {name is pet_name.title()}")
+describe_pet('dog','bruNo')
+
+
+# In[13]:
+
+
+def greet_user():
+    """Display a simple greeting"""
+print('Hello')
+greet_user()
+
+
+# In[15]:
+
+
+def greet_user(username):
+    """Display a greeting message to the user"""
+print(f"Hello, {username.title()}welcomeback")
+greet_user('shareef')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
